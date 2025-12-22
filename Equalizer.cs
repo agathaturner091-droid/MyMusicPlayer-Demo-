cd161c3a6c2f0bd833a26ef69cc0da84b6b90582
agathaturner091-droid <agathaturner091@gmail.com>
2025-12-22 16:31:56 +0800

    Add files via upload

---
+++ Equalizer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MusicPlayer
+{
+    public partial class Equalizer : Form
+    {
+        public Equalizer()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
